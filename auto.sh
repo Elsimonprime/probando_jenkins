c3a97c584a82ce70ee0f07cd2ef53665caa1c21a
Elsimonkyprime <si.vergara@duocuc.cl>
2025-04-30 18:21:07 -0400

    creacion de archivos para sitio web

---
+++ auto.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "FROM python:3.10-slim" >> Dockerfile
+echo "WORKDIR /app" >> Dockerfile
+echo "COPY app.py /app" >> Dockerfile
+echo "COPY index.html /app/templates/" >> Dockerfile
+echo "RUN pip install flask" >> Dockerfile
+echo "EXPOSE 8080" >> Dockerfile
+echo 'CMD ["python", "app.py"]' >> Dockerfile
+
+docker build -t web1 .
+docker run -d -p 8080:8080 --name web1 web1
+docker ps 
+
